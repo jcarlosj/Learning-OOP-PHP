3b06903c38b999a6f3834c4eb21b83afef37cc21
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-01 14:22:41 -0500

    Mejora legibilidad del código mensaje (Programación estructurada)

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
   /* Programación estructurada (Código spaguetti) */
   $nombrePersona1 = 'Melisa';
   $apellidoPersona1 = 'Sánchez';
+  $nombreCompleto1 = "$nombrePersona1 $apellidoPersona1";
 
   $nombrePersona2 = 'Juan';
   $apellidoPersona2 = 'Herrera';
+  $nombreCompleto2 = "$nombrePersona2 $apellidoPersona2";
 
-  echo "$nombrePersona1 $apellidoPersona1 es amiga de $nombrePersona2 $apellidoPersona2";
+  echo "$nombreCompleto1 es amiga de $nombreCompleto2";
 ?>
