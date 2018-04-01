300f7ddc0e1ab9112a973d7542f93f2992cc4ae6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-01 16:25:11 -0500

    Agrega 'Getters' para acceder a las propiedades aún cuando el modificador de acceso sea privado

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-/<?php
+<?php
   /* Programación orientada a objetos */
 
   # Clase
   class Persona {
     /* Atributos */
-    private $nombre,
+    private $nombre,                # Encapsula al cambiar el modificador de acceso
             $apellido;
 
     /* Constructor */
@@ -13,9 +13,18 @@
       $this -> apellido = $apellido;
     }
 
-    /* Función */
+    /* Método */
     public function nombreCompleto() {
       return "$this->nombre $this->apellido";
+    }
+
+    /* Getters */
+    public function getPrimerNombre() {
+      return $this -> nombre;
+    }
+
+    public function getPrimerApellido() {
+      return $this -> apellido;
     }
   }
 
@@ -24,10 +33,9 @@
   $persona2 = new Persona( 'Juan', 'Herrera' );
 
   # Mensaje
-  echo "{$persona1->nombre $persona1->apellido} es amiga de {$persona2 -> nombreCompleto()}";
+  echo "{$persona1->getPrimerNombre()} {$persona1->getPrimerApellido()} es amiga de {$persona2 -> nombreCompleto()}";
 
-  /* NOTA: En el mensaje NO podemos acceder las propiedades o atributos del objeto
-          'persona1' debido a que los mismos tienen un modificador de acceso
-          (ámbito) privado. Genera un ERROR, pero nos protege de accederlo o cambiarlo
-          directamente. */
+  /* NOTA: En el mensaje podemos acceder las propiedades o atributos del objeto
+          'persona1' haciendo uso de los Getters, métodos que permiten acceder al
+          valor contenido en una propiedad de la clase */
 ?>
