c0f095ddda02c5130c5c89d38bab286f676c50c2
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-01 14:15:10 -0500

    Imprime mensaje de amistad (Programación estructurada)

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+  /* Programación estructurada (Código spaguetti) */
+  $nombrePersona1 = 'Melisa';
+  $apellidoPersona1 = 'Sánchez';
+
+  $nombrePersona2 = 'Juan';
+  $apellidoPersona2 = 'Herrera';
+
+  echo "$nombrePersona1 $apellidoPersona1 es amiga de $nombrePersona2 $apellidoPersona2";
+?>
