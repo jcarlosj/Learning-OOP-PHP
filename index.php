b2cfbdaa11d926eedd42224c304e2833c297c2f5
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-03 21:59:00 -0500

    Implementa la autocarga de ficheros (Mostrará el primer archivo requerido por la aplicación)

--- index.php
+++ index.php
@@ -2,15 +2,12 @@
   /* Programación orientada a objetos */
   require 'src/helpers.php';
 
-  require 'src/Unidad.php';
-  require 'src/Soldado.php';
-  require 'src/Arquero.php';
-  require 'src/Armadura.php';
-  require 'src/ArmaduraBronce.php';
-  require 'src/ArmaduraPlata.php';
-  require 'src/ArmaduraMaldita.php';
+  # Implementa el 'autoload' pasando como argumento un 'CallBack' 
+  spl_autoload_register( function ( $nombreClase ) {
+    exit( $nombreClase );
+  });
 
-
+  $armadura = new ArmaduraBronce;
   # Instancia con la nueva clase 'Soldado' e inyecta una dependiencia (el objeto armadura)
   $bryan = new Soldado( 'Muñoz' ); # Sin Armadura
   /* Pasar como parámetro un objeto a otro se le llama Inyección de dependencias e indica que 
@@ -20,7 +17,7 @@
   $jhonny = new Arquero( 'Cortes' );
   $jhonny -> atacar( $bryan );
 
-  $bryan -> setArmadura( new ArmaduraMaldita ); # Con Armadura (Después del primer ataque el soldado recibe una armadura)
+  $bryan -> setArmadura( $armadura ); # Con Armadura (Después del primer ataque el soldado recibe una armadura)
 
   $jhonny -> atacar( $bryan );
   $bryan -> atacar( $jhonny );
