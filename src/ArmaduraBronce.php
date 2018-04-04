20b0be4e381f19f083fd9d9235dc7ed9e728d021
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-03 23:36:30 -0500

    Agrega 'Alias' a la importación de archivos con 'namespace'
     - Los Alias sirven para renombrar la importación de archivos
     - Se puede usar cuando por ejemplo se van a llamar dos archivos que
       se llaman igual pero pertenecen a 'namespaces' diferentes

--- src/ArmaduraBronce.php
+++ src/ArmaduraBronce.php
@@ -1,10 +1,11 @@
 <?php
   namespace Juego;
 
-  use Warcraft\Armadura;
+  use Warcraft\Armadura as ArmaduraWarcraft;
+  #use GameOfThrones\Armadura as ArmaduraGameOfThrones;
 
   # Clase 'Armadura de Bronce' (básica de bajo nivel de protección, absorbe la 1/2 del daño) que implementa la interface de una Armadura
-  class ArmaduraBronce implements Armadura {
+  class ArmaduraBronce implements ArmaduraWarcraft {
     /* Métodos (Acciones) */
     public function absorberDanio( $danio ) {
       return $danio / 2;
