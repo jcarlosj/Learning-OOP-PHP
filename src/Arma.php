da788425429d5fc001172b29954301ee4a7b72e5
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-11 16:20:15 -0500

    Agrega comentarios sobre el Patrón 'Factory' y la forma de ampliar su funcionalidad
    Aclaración
    Ampliar la funcionalidad significaría que cada arma o cada nueva arma podría tener un
    método que implemente el patrón 'Factory' instanciando una clase diferente en cada caso
    que pueda ampliar las funcionalidades de cada tipo de ataque creado.
    
    Ej: Clase 'ArcoDeFuego' que tiene el método 'crearAtaque' comentado y mediante el cual
        expande la funcionalidad de la clase padre 'Arma' para que instancie otra Clase
        a través de este patrón para crear un Ataque nuevo con otras características

--- src/Arma.php
+++ src/Arma.php
@@ -12,7 +12,22 @@
 
         /* Método */
         public function crearAtaque() {
+            # Implementación del Patrón Factory (Fabrica)
+            #   La lógica que nos permitió entender que esté patrón podría implementarse es entender que un Arma es capaz de producir múltiples ataques
+            #   Lo que no forza a la instancia a limitar la funcionalidad, si no al contrario a ampliarla
             return new Ataque( $this -> danio, $this -> magico, $this -> descripcion );
         }
+        /* NOTA: El objeto que se genera a través de esta instancia es lo que se denomina como un 'Value Object' son pequeños objetos que se usan para
+                 agrupar propiedades que tienen sentido o relación, pero no tienen sentido sueltas. Se usan comunmente para representar rangos de fecha,
+                 dinero, coordenadas entre otras
 
+                    Dinero (Cantidad y tipo de moneda):
+                        new Moneda( 50, 'USD' );
+
+                    Coordenadas (Latitud y Longitud):
+                        new Coordenadas( '38.54545N', '78.90234W' );
+
+                    Fecha (Año, Mes y Día):
+                         new Date( 206, 07, 21 );         
+                 */
     }
--- src/Armas/ArcoDeFuego.php
+++ src/Armas/ArcoDeFuego.php
@@ -9,4 +9,14 @@
         protected $danio = 30,
                   $magico = true,
                   $descripcion = ':unidad dispara una flecha de fuego a :oponente';    # Usamos 'Placeholder' (:unidad, :oponente) al adicionar los dos puntos
+
+
+        /* Ampliar la funcionalidad del Patrón 'Factory' también hace parte del modelo, por ejemplo permitir a cada
+          clase hija implementar la creación de nuevos objetos o instancias de clase */
+        /*
+        public function crearAtaque() {
+            return new AtaqueFlechaDeFuego;        # Creando una nueva clase para que se instancie a través del patrón ampliando la funcionalidad
+        }
+        */
+
     }
