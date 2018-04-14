131e57909f65acfd9bcc46ece8281d787103b11f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-14 11:53:20 -0500

    Convierte la propiedad (o atributo) 'nombre' en una propiedad estática 'static'
    En este ejemplo se accede al contenido de la propiedad estática 'nombre' a través
    de un 'Getter' método de la clase
    
    Aclaración
    
    Cuando una propiedad o atributo de una clase es declarada como estática (static)
    esta ya no está disponible como parte de cada instancia de cada objeto. Ahora es
    parte global de la clase como tal, no de las instancias de cada objeto que se
    pueda crear, la manera de accederla no será a través de una instancia si no del
    uso de los :: dobles puntos.

--- public_html/Persona.php
+++ public_html/Persona.php
@@ -5,16 +5,16 @@
  */
 class Persona {
     /* Propiedades (Atributos) */
-    protected $nombre;
+    protected static $nombre;
 
     /* Constructor */
     public function __construct( $nombre ) {
-        $this -> nombre = $nombre;
+        static :: $nombre = $nombre;
     }
 
     /* Getters */
     public function getNombre() {
-        return $this -> nombre;
+        return static :: $nombre;
     }
 
 }
@@ -23,4 +23,7 @@ class Persona {
 $melisa = new Persona( 'Melisa' );
 $alejandro = new Persona( 'Alejandro' );
 
-echo "<p>{$melisa->getNombre()} y {$alejandro->getNombre()}</p>";
+echo "<p>{$melisa->getNombre()} y {$alejandro->getNombre()}</p>";    // Alejandro y Alejandro
+
+/* NOTA: Esto pasa por que la propiedad 'nombre' ya no está disponible como parte de cada instancia de cada objeto.
+         Ahora es parte global de la clase como tal, no de las instancias de cada objeto que se pueda crear */
