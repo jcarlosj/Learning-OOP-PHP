9866d3fb77d30ceb98d4911f2ae7b1804770e2ce
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-14 17:21:49 -0500

    Refactoriza la funcionalidad de mensajes de ataque dinámicos

--- src/Traducir.php
+++ src/Traducir.php
@@ -13,18 +13,26 @@
         public static function get( $id_mensaje, array $parametros = array() ) {
 
             # Valida si el mensaje NO existe
-            if( ! isset( static :: $mensajes[ $id_mensaje ] ) ) {
+            if( ! static :: has( $id_mensaje ) ) {
                 return "[ $id_mensaje ]";
             }
 
-            # Existe mensaje
-            $mensaje = static :: $mensajes[ $id_mensaje ];    # Asigna mensaje a una variable temporal local al método
-
-            # Recorre los paŕametros para reemplazar para uno por su valor
-            foreach ( $parametros as $key => $value ) {
-                $mensaje = str_replace( ":$key", $value, $mensaje );
-            }
-
-            return $mensaje;
+            return static :: reemplazarParametros( static :: $mensajes[ $id_mensaje ], $parametros );
         }
+
+         # Función que Valida si el mensaje NO existe un mensaje
+         private static function has( $id_mensaje ) {
+             return isset( static :: $mensajes[ $id_mensaje ] );
+         }
+
+         # Función que reeemplaza los Parámetros del mensaje
+         private static function reemplazarParametros( $mensaje, array $parametros ) {
+
+             # Recorre los paŕametros para reemplazar para uno por su valor
+             foreach ( $parametros as $key => $value ) {
+                 $mensaje = str_replace( ":$key", $value, $mensaje );
+             }
+
+             return $mensaje;
+         }
     }
