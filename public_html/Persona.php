79a7abdb408f72a814066fa4a69f4fcee37e4b4a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-14 11:59:27 -0500

    Accede a la propiedad (o atributo) estático a través del nombre de la clase
    Aclaración
    Se puede acceder a la propiedad estática de la clase siempre que su visibilidad
    sea pública (public) y se hace usando el nombre de la clase el uso del operador
    :: dos puntos dobles y el nombre de la propiedad. No se requiere instanciar la
    clase.

--- public_html/Persona.php
+++ public_html/Persona.php
@@ -5,25 +5,7 @@
  */
 class Persona {
     /* Propiedades (Atributos) */
-    protected static $nombre;
-
-    /* Constructor */
-    public function __construct( $nombre ) {
-        static :: $nombre = $nombre;
-    }
-
-    /* Getters */
-    public function getNombre() {
-        return static :: $nombre;
-    }
-
+    public static $nombre = 'Melisa';
 }
 
-/* Instancias */
-$melisa = new Persona( 'Melisa' );
-$alejandro = new Persona( 'Alejandro' );
-
-echo "<p>{$melisa->getNombre()} y {$alejandro->getNombre()}</p>";    // Alejandro y Alejandro
-
-/* NOTA: Esto pasa por que la propiedad 'nombre' ya no está disponible como parte de cada instancia de cada objeto.
-         Ahora es parte global de la clase como tal, no de las instancias de cada objeto que se pueda crear */
+exit( Persona :: $nombre );
