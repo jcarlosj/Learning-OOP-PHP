993dd79eaa9dd380ed96b1beb80df7dfe21dd278
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-14 12:33:57 -0500

    Ejemplo: propiedades estáticas interactuando con Instancias de Clase

--- public_html/Persona.php
+++ public_html/Persona.php
@@ -5,18 +5,34 @@
  */
 class Persona {
     /* Propiedades (Atributos) */
-    protected static $nombre = 'Melisa';
+    protected $nombre;
+    public static $database = 'mysql';       # Supuesto nombre de la base de datos
+    public static $db_table = 'personas';    # Supuesto nombre de tabla en la base de datos
+    /* NOTA: al declararla estática para asumir que todas las personas se registrarán en la misma tabla,
+             al igual que una base de datos */
 
     /* Constructor */
     public function __construct( $nombre ) {
-        static :: $nombre = $nombre;
+        $this -> nombre = $nombre;
     }
 
-    /* Getter Static */
-    public static function getNombre() {
-        return static :: $nombre;
+    /* Getters */
+    public function getNombre() {
+        return $this -> nombre;
     }
 
 }
 
-exit( Persona :: getNombre() );        // Melisa
+/* Instancias */
+$melisa = new Persona( 'Melisa' );
+$alejandro = new Persona( 'Alejandro' );
+
+echo "<p>{$melisa->getNombre()} se ha guardado en la tabla <b>'" .Persona::$db_table. "'</b></p>";
+echo "<p>{$alejandro->getNombre()} se ha guardado en la tabla <b>'" .Persona::$db_table. "'</b></p>";
+
+$juan_david = new Persona( 'Juan David' );
+Persona :: $db_table = 'desarrolladores';
+echo "<p>{$juan_david->getNombre()} se ha guardado en la tabla <b>'" .Persona::$db_table. "'</b></p>";
+
+/* NOTA: Esto pasa por que la propiedad 'nombre' ya no está disponible como parte de cada instancia de cada objeto.
+         Ahora es parte global de la clase como tal, no de las instancias de cada objeto que se pueda crear */
