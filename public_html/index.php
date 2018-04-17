026b4ad5a912b48368c54eb7446f0072d7761414
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-17 13:51:21 -0500

    Refactoriza e implementa buenas prácticas para métodos mágicos. Agrega y despliega nueva propiedad

--- public_html/index.php
+++ public_html/index.php
@@ -10,7 +10,11 @@
   $usuario = new Usuario([
       # Crea 2 propiedades y asigna el valor
       'primer_nombre'    => 'Elisa',
-      'primer_apellido'  => 'Giraldo'
+      'primer_apellido'  => 'Giraldo',
+      'nombre_usuario'   => '@egiraldo'
   ]);
 
-  echo "<p>Hola, {$usuario->primer_nombre} {$usuario->segundo_nombre} {$usuario->primer_apellido} {$usuario->segundo_apellido} Bienvenida!</p>";        # Despliega el valor de la propiedad creada dinámicamente
+  # Despliega un atributo usando el método que contiene la lógica de validación para obtener el valor de una propiedad
+  echo "Nickname: {$usuario->getAtributo('nombre_usuario')}";
+  # Despliega el solo el valor de la propiedades creada dinámicamente que son validas usando el método mágico de PHP '__get()'
+  echo "<p>Hola, {$usuario->primer_nombre} {$usuario->segundo_nombre} {$usuario->primer_apellido} {$usuario->segundo_apellido} Bienvenida!</p>";
--- src/Usuario.php
+++ src/Usuario.php
@@ -12,13 +12,19 @@
             $this -> atributos = $atributos;
         }
 
-        /* Métodos Mágicos de PHP */
-        public function __get( $nombre_propiedad ) {
+        /* Obtiene el valor de una propiedad */
+        public function getAtributo( $nombre_propiedad ) {
 
             if( array_key_exists( $nombre_propiedad, $this -> atributos ) ) {
                 # En caso de existir el campo del 'Array' retornamos el valor contenido
                 return $this -> atributos[ $nombre_propiedad ];
             }
             # En caso de no existir el campo del 'Array' PHP por defecto retorna null
+        }
+
+        /* Métodos Mágicos de PHP */
+        public function __get( $nombre_propiedad ) {
+            return $this -> getAtributo( $nombre_propiedad );
+            /* NOTA: Es una buena práctica extraer la lógica de los métodos mágicos y delegarsela a un método corriente */
         }
     }
