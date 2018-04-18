6273b54f6e6edc0d4dab48dd9390344b1e8e5063
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-18 10:47:35 -0500

    Crea la clase 'UnaClase' e invoca al método inexistente en la misma que induce al ERROR)

--- public_html/index.php
+++ public_html/index.php
@@ -2,26 +2,10 @@
   /* Programación orientada a objetos */
   namespace MetodosMagicos;
 
-  use MetodosMagicos\Usuario;
-
   # Implementa el 'autoload' generado por 'Composer' (para cargar las clases del proyecto automáticamente)
   require '../vendor/autoload.php';
 
-  $usuario = new Usuario();
-  $usuario -> setAtributos([
-      # Crea 2 propiedades y asigna el valor
-      'primer_nombre'    => 'Elisa',
-      'primer_apellido'  => 'Giraldo'
-  ]);
+  class UnaClase {}             # Crea una clase sin métodos definidos
 
-  $usuario -> setAtributo( 'nombre_usuario', '@egiraldo' );
-  unset( $usuario -> nombre_usuario );    # Como las propiedades son dinámicas y mágicas, requiere el método mágico __unset() para funcionar
-
-  # Despliega el solo el valor de la propiedades creada dinámicamente que son validas usando el método mágico de PHP '__get()'
-  echo "<p>Hola, {$usuario->primer_nombre} {$usuario->segundo_nombre} {$usuario->primer_apellido} {$usuario->segundo_apellido} Bienvenida!</p>";
-
-  # Valida si el usuario tiene un Nickname y lo muestra en caso de tenerlo
-  if( isset( $usuario -> nombre_usuario ) ) {
-      # Despliega un atributo usando el método que contiene la lógica de validación para obtener el valor de una propiedad
-      echo "Nickname: {$usuario->getAtributo('nombre_usuario')}";
-  }
+  $unObjeto = new UnaClase;     # Instancia la clase
+  $unObjeto -> unMetodo();      # Invoca un método que no existe en la clase
