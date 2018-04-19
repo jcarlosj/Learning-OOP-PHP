6da4561c5701dfa4971c3c164b4fe371ce35c02c
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-19 10:51:09 -0500

    Obtener los atributos del elemento, retorna 'null' si el elemento no existe
    Aplica tanto para los métodos llamados a través de instancias de clase y
    los Objetos con métodos estáticos

--- public_html/index.php
+++ public_html/index.php
@@ -17,24 +17,5 @@
 
 # Agrega atributo 'name' al elemento HTML, invocando al método inexistente 'name'
   echo $elemento -> render();
-  #echo '<br /><pre>'; var_dump( $elemento ); echo '</pre>';
-
-  echo '<h3>Forma 2</h3><p>Método estático e interface fluída</p>';
-  # Método estático
-  $elemento2 = NodoHTML :: tag_no_permitido( 'Borre este texto' )
-                  -> id( 'descripcion-2' )
-                  -> class( 'persona-mensaje' )
-                  -> placeholder( 'Escriba una breve descripción personal' );
-
-  # Agrega atributo 'name' al elemento HTML, invocando al método inexistente 'name'
-    echo $elemento2;                    # Forma 1: Implementa el método mágico para que pueda imprimirse como una cadena, dejando que PHP lo haga por si mismo.
-    echo '<br /><pre>'; var_dump( $elemento2 ); echo '</pre>';
-
-    echo $elemento2 -> __toString();    # Forma 2: Llamar de forma explicita al método mágico para que pueda imprimirse como una cadena
-    echo '<br /><pre>'; #var_dump( $elemento2 -> __toString() ); echo '</pre>';
-
-    echo (string) $elemento2;                       # Forma 3: Castear el objeto
-    echo '<br /><pre>'; #var_dump( (string) $elemento2 ); echo '</pre>';
-    /* NOTA: Una ves el objeto se convierte en una cadena en este caso no podemos revertirla */
-
-    echo "<p>Y el programa continua...</p>";
+  echo '<br /><pre>'; var_dump( $elemento ); echo '</pre>';
+  echo '<br /><pre>'; var_dump( $elemento -> get( 'name' ), $elemento -> get( 'width' ) ); echo '</pre>';
--- src/NodoHTML.php
+++ src/NodoHTML.php
@@ -40,22 +40,11 @@
         /* Convierte un objeto en una cadena */
         public function __toString() {
 
-            /* NOTA: __toString() no debe lanzar una Exception dentro de si en ocasiones
-                     no nos permitirá ver el mensaje personalizado de la misma. En caso
-                     de que no se despliegue el mensaje personalizado de la Exception se
-                     recomienda llamar temporalmente de forma explicita al objeto (forma 2)
-                     para que despliegue este mensaje */
-
             return $this -> render();        # Imprime el elemento HTML
         }
 
         /* Devuelte el elemento HTML estructuralmente conformado */
         public function render() {
-
-            # Lanza Excepción elemento no permitido
-            if( $this -> tag == 'tag_no_permitido' ) {
-                throw new \Exception( "El elemento que esta usando no es valido" , 1 );
-            }
 
             $resultado = "<$this->tag {$this->encadenarAtributos()}>";
 
@@ -78,6 +67,13 @@
             }
 
             return $resultado;
+        }
+
+        public function get( $nombre_atributo ) {
+
+            return isset( $this -> attributes[ $nombre_atributo ] )
+                    ? $this -> attributes[ $nombre_atributo ]
+                    : null;
         }
 
     }
