f13059d4a53e20dc8898bf720089952d66077689
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-19 11:37:57 -0500

    Obtiene el nombre con el que se ha instanciado el objeto 'Usuario'

--- public_html/index.php
+++ public_html/index.php
@@ -7,15 +7,6 @@
   # Implementa el 'autoload' generado por 'Composer' (para cargar las clases del proyecto automáticamente)
   require '../vendor/autoload.php';
 
-  echo '<h3>Forma 1</h3><p>Método a través de Instancia de clase e interface fluída</p>';
-  # Método a través de Instancia de clase
-  $elemento = ( new NodoHTML( 'textarea', 'Borre este texto' ))
-              -> name( 'contenido' )                        # Corrige
-              -> id( 'descripcion' )                        # Agrega nueva propiedad
-              -> class( 'mensajes' )                        # Agrega nueva propiedad
-              -> placeholder( 'Escriba un mensaje aquí' );  # Agrega nueva propiedad
+  $usuario = new Usuario([ 'nombre' => 'Elisa' ]);
 
-# Agrega atributo 'name' al elemento HTML, invocando al método inexistente 'name'
-  echo $elemento -> render();
-  echo '<br /><pre>'; var_dump( $elemento ); echo '</pre>';
-  echo '<br /><pre>'; var_dump( $elemento( 'name' ), $elemento( 'width', 100 ) ); echo '</pre>';    # Elimina el llamado explicito al método 'get'
+  echo $usuario -> getAtributo( 'nombre' );
