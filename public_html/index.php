9fb64ffa808f168653e346b593d8bea8afc2fbd2
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-19 17:28:39 -0500

    Crea 2 objetos 'Usuario' que usan como parámetro el mismo 'Array'

--- public_html/index.php
+++ public_html/index.php
@@ -7,17 +7,17 @@
   # Implementa el 'autoload' generado por 'Composer' (para cargar las clases del proyecto automáticamente)
   require '../vendor/autoload.php';
 
-  $usuario = new Usuario([ 'nombre' => 'Elisa', 'email' => 'egiraldo@correo.co' ]);
+  # Hermanos
+  $juliana = new Usuario([ 'nombre' => 'Juliana' ]);
+  $paulaAndrea = new Usuario([ 'nombre' => 'Paula Andrea' ]);
 
-  echo '<h2>Explora el Objeto creado</h2>';
-  echo '<pre>'; var_dump( $usuario ); echo '</pre>';
+  # Lonchera
+  $lonchera = [ 'emparedado' ];
 
-  # Serializa el objeto (como si se tratara de una cadena)
-  $data = serialize( $usuario );                                # En este instante se ejecuta el método mágico __wakeup()
-  echo '<h2>Datos serializados y guardados</h2>' .$data;
+  # Asigna el almuerzo de la lonchera a los Hermanos
+  $juliana -> setAlmuerzo( $lonchera );
+  $paulaAndrea -> setAlmuerzo( $lonchera );
 
-  /* Guarda los datos serializados en un archivo  */
-  file_put_contents( '../storage/usuarios.txt', $data );
-
-  echo '<p><a href="datos.php">Obtener datos</a></p>';
-  /* NOTA: También se puede usar la función serializable() de PHP para serializar y deserializar datos en PHP */
+  # Las Alumnas comen el almuerzo
+  $juliana -> come();
+  $paulaAndrea -> come();
--- src/Usuario.php
+++ src/Usuario.php
@@ -6,32 +6,24 @@
     class Usuario extends Model {
 
         /* Propiedades (Atributos)*/
-        private $claveBaseDatos = 'secreto';        # Un atributo de este tipo no debería ir aquí, pero es parte del ejemplo
-        private $nombreBaseDatos = 'DB_TiendaVirtual';
+        protected $almuerzo;
 
-        /* Métodos mágicos */
-        public function __sleep() {
-            /* Esta función mágica, se ejecuta luego de la serialización */
-            echo '<p style="color: blue;">Serializando...</p>';
-
-            return [ 'atributos' ];        # Retorna solo las propiedades que se desean visualizar ($atributos es una propiedad de la clase padre)
+        /* Asigna el almuerzo a un usuario */
+        public function setAlmuerzo( $almuerzo ) {
+            $this -> almuerzo = $almuerzo;
         }
-        public function __wakeup() {
-            /* Esta función mágica, se ejecuta luego de la deserialización */
-            echo '<p style="color: blue;">Deserializando...</p>';
 
-            echo '<h2 style="color:red;">Objeto deserializado</h2>';
-            echo '<pre>'; var_dump( $this ); echo '</pre>';
+        /* */
+        public function come() {
 
-            echo '<h2 style="color:red;">Imprime valores del objeto</h2>';
-            echo '<p><b>Nombre:</b> '. $this -> getAtributo( 'nombre' ). '<br /><b>Correo:</b> ' .$this -> getAtributo( 'email' ). '</p>';
-        }
-        /* NOTA: Eloquent usa esta técnica usando los métodos mágicos: __sleep(), __wakeup()
-                 serializando/deserializando exclusivamente el ID */
+            /* NOTA: Es importante dejar las validaciones y/o Excepciones en la primera parte de cada método que la requiera */
+            if( empty( $this -> almuerzo ) ) {
+                throw new \Exception( "{$this->getAtributo('nombre')} no tiene nada para comer :( ", 1);
+            }
 
-        /* Método Dinámico */
-        public function getNombreAtributo( $valor ) {
+            /* Despúes Lógica */
+            $comida = array_shift( $this -> almuerzo );                        # 'array_shift' función de PHP para extraer y eliminar el primer elemento de un 'Array'
+            echo "<p>{$this->getAtributo('nombre')} almuerza {$comida}</p>";
 
-            return strtoupper( $valor );        # Retorma el valor en mayúsculas
         }
     }
