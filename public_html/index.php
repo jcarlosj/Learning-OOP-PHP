3b5613260af15b76bd2d0e753f2e450104a5697e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-19 18:22:52 -0500

    Implementa el método mágico __clone() permitiendo cambios en las propiedades del objeto clonado

--- public_html/index.php
+++ public_html/index.php
@@ -13,14 +13,16 @@
 
   # Lonchera es ahora una instancia
   $lonchera = new Lonchera([ 'emparedado', 'manzana' ]);    # Objeto 1:
+  $lonchera2 = clone( $lonchera );
 
   # Asigna el almuerzo de la lonchera a los Hermanos (la misma lonchera)
   $juliana -> setAlmuerzo( clone( $lonchera ) );            # Objeto 2: Paso parámetro por valor (objeto copia del original)
-  $paulaAndrea -> setAlmuerzo( clone( $lonchera ) );        # Objeto 3: Paso parámetro por valor (objeto copia del original)
+  $paulaAndrea -> setAlmuerzo( clone( $lonchera2 ) );        # Objeto 3: Paso parámetro por valor (objeto copia del original)
 
   # Las Alumnas comen el almuerzo
   $juliana -> come();
   $juliana -> come();
   $paulaAndrea -> come();
 
-  echo '<pre><b>Lonchera Original:</b> '; var_dump( $lonchera ); echo '</pre>';
+  echo '<pre><b>Lonchera 1:</b> '; var_dump( $lonchera ); echo '</pre>';
+  echo '<pre><b>Lonchera 2 (Clonada):</b> '; var_dump( $lonchera2 ); echo '</pre>';
--- src/Lonchera.php
+++ src/Lonchera.php
@@ -3,13 +3,26 @@
 
     class Lonchera {
         /* Propiedades (Atributos) */
-        protected $comida = [];
+        protected $comida = [];                /* NOTA: Es una muy buena idea envolver un 'Array' dentro de una clase para ampliar las funcionalidades
+                                                        esto nos acerca al concepto de las Colecciones en PHP */
+        protected $original = true;
+        protected $estado = 'Alimento para consumir';
 
         /* Constructor */
         public function __construct( array $comida = [] ) {
             $this -> comida = $comida;
         }
 
+        /* Método mágico */
+        public function __clone() {
+            /* Cuando este método detecte que va a ser clonado va a realizar
+               las instrucciones o cambios que se encuentren dentro de él, y
+               el objeto clonado será diferente */
+            $this -> original = false;
+            $this -> estado = 'Alimento vencido';
+        }
+
+        /* Métodos */
         public function consumir() {
 
             return array_shift( $this -> comida );
