8f3ee474e2ae8ab25228b756f4dfdbbbfd445fea
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-25 09:33:32 -0500

    Comparación entre objetos que representan a una entidad 'No Value Object'
    Los objetos que representan entidades deben poseer identificadores propios
    representados por alguna o varias propiedades, en este caso son esas propiedades
    las que definen a los objetos como los mismos a diferencia de los 'Value Objects'

---
+++ public_html/Persona.php
@@ -0,0 +1,29 @@
+<?php
+    /* Programación orientada a objetos */
+    namespace MetodosMagicos;
+
+    use MetodosMagicos\NodoHTML;
+
+    # Implementa el 'autoload' generado por 'Composer' (para cargar las clases del proyecto automáticamente)
+    require '../vendor/autoload.php';
+
+    class Persona {
+      public $nombre;
+      public $enlinea = false;
+      public $id;
+
+      public function __construct( $nombre ) {
+          $this -> nombre = $nombre;
+      }
+    }
+
+    # Objetos que representan a una entidad (No 'Value Objects')
+    $elisa = new Persona( 'Elisa' );            # Instancia 1
+    $elisa -> enlinea = true;                   # Estado
+    $elisa -> id = 1;                           # ID del objeto
+
+    $elisa2 = new Persona( 'Elisa' );           # Instancia 2
+    $elisa2 -> id = 1;                           # ID del objeto
+
+    // En este caso las validaciones hay que hacerlas a las propiedades más relevantes del objeto y no entre los simples objetos entre sí
+    echo ( $elisa -> id == $elisa2 -> id ) ? 'VERDADERO' : 'FALSO';
