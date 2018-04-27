7c3c50b030c475807fd23ea67c0a784cb88f8e95
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-27 12:11:29 -0500

    Crea e implementa una 'macro' (Método no existente creado dinámicamente)
    Explicación:
    La 'Macro' es un concepto de 'Laravel' que permite la creación de métodos
    que no existen dentro de una clase de forma dinámica, de manera que
    podamos utilizarlos para mejorar la funcionalidad de un paquete.
    
    Usualmente se aplica para evitar tener que modificar los paquetes de
    terceros al intentar ampliar una funcionalidad o agregar una que no
    posee. Modificar los paquetes de terceros no es una práctica recomendada
    por eso Laravel propone el uso de 'Macros' para lograr este propósito
    
    Aclaración:
    La funcionalidad de la macro es desarrollada dentro del paquete externo
    que se ha pasado a 'vendor' y que propone implementar esta macro desde
    cualquier parte de la aplicación.

--- public_html/index.php
+++ public_html/index.php
@@ -6,6 +6,18 @@
   require '../vendor/autoload.php';
   require '../vendor/paquete_externo/HTMLBuilder.php';        # Por ahora no lo incluimos en el 'autoload' con Composer
 
+  use paquete_externo\HTMLBuilder;
+
+  # Agrega un método de forma dinámica (Crea una macro)
+  HTMLBuilder :: macro( 'success', function( $message ) {
+
+      return "<p style='background-color: #DFF0D8; border-color: #D0E9C6; color: #3C763D; padding: 10px;'>{$message}</p>";
+  });
+  /* NOTA: En Laravel este concepto de crear métodos de forma dinámica se le conoce como MACROS
+           En Laravel estas macros puede agregarsele al constructor de HTML, al de formularios,
+           Las colecciones del Frameworks, etc. */
+
   $html = new HTMLBuilder();
+
   echo $html -> hr();
-  echo $html -> success( 'Todo salió bien!' );
+  echo $html -> success( 'Todo salió bien!' );        # Aquí se hace el llamado al método dinámico (o macro creada)
