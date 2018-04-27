2d6ef5b5bdd4de8291dfbcbad42b680e6264dc53
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-27 12:49:07 -0500

    Llamar un método existente dentro del contexto de un Macro o Método dinámico (Induce al ERROR)

--- public_html/index.php
+++ public_html/index.php
@@ -12,13 +12,11 @@
   # Agrega un método de forma dinámica (Crea una macro)
   HTMLBuilder :: macro( 'success', function( $message ) {
 
-      return "<p style='background-color: #DFF0D8; border-color: #D0E9C6; color: #3C763D; padding: 10px;'>{$message}</p>";
+      return "<p style='background-color: #DFF0D8; border-color: #D0E9C6; color: #3C763D; padding: 10px;'>{$message}</p>" . $this -> hr();
   });
   /* NOTA: En Laravel este concepto de crear métodos de forma dinámica se le conoce como MACROS
            En Laravel estas macros puede agregarsele al constructor de HTML, al de formularios,
            Las colecciones del Frameworks, etc. */
 
   $html = new HTMLBuilder();
-
-  echo $html -> hr();
   echo $html -> success( 'Todo salió bien!' );        # Aquí se hace el llamado al método dinámico (o macro creada)
